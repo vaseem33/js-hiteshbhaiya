11bb93443e372bbc5424e142a09dc3a306d57a9f
vaseem33 <122249547+vaseem33@users.noreply.github.com>
2025-10-13 04:15:47 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month="march"
+switch (month) {
+    case "jan":
+        console.log("January");    
+        break;
+    case "feb":
+        console.log("Febrauary");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+    default:
+        console.log("deafult case match");
+        
+        break;
+}
