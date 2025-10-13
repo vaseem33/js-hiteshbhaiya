20717d0e0db5c7a36e8191a0a23585889f72041f
vaseem33 <122249547+vaseem33@users.noreply.github.com>
2025-10-13 05:05:13 +0000

    three

---
+++ 05_iterations/three.js
@@ -0,0 +1,34 @@
+const myObject={
+    js:"javascript",
+    cpp:"c++",
+    rb:"ruby",
+    swift:"swift by apple"
+}
+
+// for (const key in myObject) {
+// // console.log(myObject[key]);
+
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+    
+// }
+
+const programming=["js","rb","py","java","cpp"]
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+    
+    
+// }
+const map=new Map()
+map.set("IN","INDIA")
+map.set("USA","UNITED STATES OF AMERICA")
+map.set("FR","France")
+map.set("IN","INDIA")
+
+for (const key in map) {
+    
+    console.log(key);
+    
+    
+}
